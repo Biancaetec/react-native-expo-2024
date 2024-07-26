a8214c1e266e1ff3cef227191b940043eec6f522
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-07-26 15:26:10 +0000

    [feat] Projeto ddm

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha orimeira janela</Text>
+      <Text>Minha primeira janela</Text>
       <StatusBar style="auto" />
     </View>
   );
