01c61609260d80a4a3ef5bea2f54d48ff1843bba
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-08-04 20:37:21 +0000

    [feat] Expo-font with Context

--- src/hooks/Font/index.js
+++ src/hooks/Font/index.js
@@ -1,33 +1,40 @@
 import { useFonts } from "expo-font";
 import { createContext, useContext } from "react";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator,Text, View } from "react-native";
 
 const FontContext = createContext({});
 
 export function FontProvider({children}) {
 
     const [loaded, error] = useFonts({
-        regular: require("../../assets/fonts/BonaNovaSC-Regular.ttf"),
-        regular: require("../../assets/fonts/Montserrat-Regular.ttf"),
-        ligth: require("../../assets/fonts/Montserrat-Light.ttf"),
-        medium: require("../../assets/fonts/OpenSans-Medium.ttf"),
-        light: require("../../assets/fonts/OpenSans-Light.ttf"),
-        regular: require("../../assets/fonts/Roboto-Regular.ttf"),
-        ligth: require("../../assets/fonts/Roboto-Light.ttf"),
-        medium: require("../../assets/fonts/Roboto-Medium.ttf"),
+        BonaNovaSCRegular: require("../../assets/fonts/BonaNovaSC-Regular.ttf"),
+        MontserratRegular: require("../../assets/fonts/Montserrat-Regular.ttf"),
+        MontserratLight: require("../../assets/fonts/Montserrat-Light.ttf"),
+        OpenSansMedium: require("../../assets/fonts/OpenSans-Medium.ttf"),
+        OpenSansLight: require("../../assets/fonts/OpenSans-Light.ttf"),
+        RobotoRegular: require("../../assets/fonts/Roboto-Regular.ttf"),
+        RobotoLight: require("../../assets/fonts/Roboto-Light.ttf"),
+        RobotoMedium: require("../../assets/fonts/Roboto-Medium.ttf"),
     });
 
     if (!loaded && !error) {
-       return <ActivityIndicator />;
+       return (
+        <View style= {{flex:1, justifyContent:"center", alignItems: "center"}}>
+            <Text style={{fontSize: 28, marginTop: 15}}> 
+                Carregando as fontes
+            </Text>
+            <ActivityIndicator size="large" color= "#0000ff"/> 
+       </View>
+      );
       }
 
-    return <FontContext.Provider value={{}}> {children} </FontContext.Provider>;
-}
+      return <FontContext.Provider value={{ loaded }}>{children}</FontContext.Provider>;
+    }
 
 export function useFont() {
     const context = useContext(FontContext);
     if(!context) {
-        throw new Error("usefont must be used within a FontProvider");
+        throw new Error("useFont must be used within a FontProvider");
     }
     return context;
 }
