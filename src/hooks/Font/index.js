eecfd0c48a1a690303a89066a80a08974675a950
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-08-04 19:11:28 +0000

    First Commit

--- src/hooks/Font/index.js
+++ src/hooks/Font/index.js
@@ -1,15 +1,33 @@
+import { useFonts } from "expo-font";
 import { createContext, useContext } from "react";
+import { ActivityIndicator } from "react-native";
 
 const FontContext = createContext({});
 
-export function FontProvider((children)) {
+export function FontProvider({children}) {
+
+    const [loaded, error] = useFonts({
+        regular: require("../../assets/fonts/BonaNovaSC-Regular.ttf"),
+        regular: require("../../assets/fonts/Montserrat-Regular.ttf"),
+        ligth: require("../../assets/fonts/Montserrat-Light.ttf"),
+        medium: require("../../assets/fonts/OpenSans-Medium.ttf"),
+        light: require("../../assets/fonts/OpenSans-Light.ttf"),
+        regular: require("../../assets/fonts/Roboto-Regular.ttf"),
+        ligth: require("../../assets/fonts/Roboto-Light.ttf"),
+        medium: require("../../assets/fonts/Roboto-Medium.ttf"),
+    });
+
+    if (!loaded && !error) {
+       return <ActivityIndicator />;
+      }
+
     return <FontContext.Provider value={{}}> {children} </FontContext.Provider>;
 }
 
 export function useFont() {
     const context = useContext(FontContext);
     if(!context) {
-        throw new Error("usefont not be used within a FontProvider")
+        throw new Error("usefont must be used within a FontProvider");
     }
     return context;
 }
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,5 @@
 import { FontProvider } from "./Font";
 
-export function AppProvider({children}) {
-    return <FontProvider>{children}</FontProvider>;
+export function AppProvider({children}) { //vai devolver todos os hoolks e font que precisamos 
+    return<FontProvider>{children}</FontProvider>;
 }
