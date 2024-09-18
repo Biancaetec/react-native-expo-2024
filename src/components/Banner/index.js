2de4625d754f83aadcc3622d7a8fa19ba4ca0876
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-09-18 23:40:51 +0000

    [feat]estilizando banner pt1

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import PagerView from "react-native-pager-view";
 
 export function Banner() {
@@ -10,37 +10,39 @@ export function Banner() {
     };
 
     return (
-    <View style={styles.container}>
-        <PagerView 
-        initialPage={0} 
-        style={styles.content} 
-        onPageSelected={onPageSelected}
-        >
-            <View key="1" style={styles.page}>
-                <Text style={styles.text}>PROMOÇÃO</Text> 
-                <Text style={styles.text}>Banner 1 - img</Text> 
+        <View style={styles.container}>
+            <PagerView 
+                initialPage={0} 
+                style={styles.content} 
+                onPageSelected={onPageSelected}
+            >
+                <View key="1" style={styles.page}>
+                    <Text style={styles.text}>PROMOÇÃO</Text> 
+                
+                </View>
+                <View key="2" style={styles.page}>
+                    <Text style={styles.text}>Hidratante</Text> 
+                    <Image 
+                        source={require('../../../src/assets/images/sabonetecereja.png')} 
+                        style={styles.image2}
+                    />
+                </View>
+                <View key="3" style={styles.page}>
+                    <Text style={styles.text}>Sabonete Líquido</Text> 
+                    <Image 
+                        source={require('../../../src/assets/images/hidratante.png')} 
+                        style={styles.image3}
+                    />
+                </View>
+            </PagerView>
+            <View style={styles.bulletContent}>
+                <View style={[styles.bullet, page === 0 && styles.activeBullet]}></View>
+                <View style={[styles.bullet, page === 1 && styles.activeBullet]}></View>
+                <View style={[styles.bullet, page === 2 && styles.activeBullet]}></View>
             </View>
-            <View key="2" style={styles.page}>
-                <Text style={styles.text}>Hidratante</Text> 
-                <Text style={styles.text}>Banner 2 - img</Text>
-            </View>
-            <View key="3" style={styles.page}>
-                <Text style={styles.text}>Sabonete Líquido</Text> 
-                <Text style={styles.text}>Banner 3 - img</Text>
-            </View>
-        </PagerView>
-        <View style={styles.bulletContent}>
-            <View style={[styles.bullet, page === 0 && styles.activeBullet]}></View>
-            <View style={[styles.bullet, page === 1 && styles.activeBullet]}></View>
-            <View style={[styles.bullet, page === 2 && styles.activeBullet]}></View>
-
         </View>
-    </View>
-  
-   
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -48,7 +50,7 @@ const styles = StyleSheet.create({
     },
     content: {
         marginTop: 20,
-        height: 220,
+        height: 230,
         alignItems: "center",
         justifyContent: "center",
         width: "100%",
@@ -58,12 +60,27 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: "#20B2AA",
         padding: 10,
-      },
+        borderRadius: 24,
+        margin: 8,
+    },
+    image2: {
+        width: "100%",
+        height: 150,
+        borderRadius: 16,
+        resizeMode: "cover",
+    },
+    image3: {
+        width: "100%",
+        height: 280,
+        borderRadius: 16,
+        resizeMode: "cover",
+        marginBottom: "-24%",
+        marginLeft: "-30%",
+    },
     bulletContent: {
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        
     },
     bullet: {
         width: 10,
