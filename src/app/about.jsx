5fd46ec15bb4eca55d7aaf0dc58a26e8f85316cf
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-09-21 17:04:06 +0000

    [feat]estilizando

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -1,49 +1,93 @@
 import { router } from "expo-router";
-import { Button, Text, View, StyleSheet, Image } from "react-native";
+import { Button, Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function About() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.container}>
             <Image
                 source={require('../assets/images/logocabanni.png')}
-                style={{
-                    marginTop: "-90%",
-                    width: '100%', 
-                    height: undefined,
-                    aspectRatio: 1, // Preserva a proporção da imagem
-                    resizeMode: 'contain',
-                }}
+                style={styles.image}
             />
-            <View>
             <Text style={styles.titulo}>
-                Seja bem-vindo !
-                </Text>
-            </View>
+                SEJA BEM-VINDO!
+            </Text>
+
             <View style={styles.retangulo}>
                 <Text style={styles.frase}>
-                    No aplicativo Cabanni, você encontrará uma seleção refinada de produtos da linha Charmm, incluindo hidratantes e sabonetes exclusivos que elevam a sua experiência de cuidado pessoal.
+                    O aplicativo Cabanni traz uma seleção exclusiva que aprimora a experiência de cuidado pessoal.
                 </Text>
             </View>
-            <Button title="Voltar" onPress={() => { router.replace("/") }} />
+            <View style={styles.sobre}>
+                 <Text style={styles.vermais}>
+                O que você encontrará :
+                </Text>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={() => { router.replace("/") }}>
+                <Text style={styles.buttonText}>Voltar</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    frase: {
-        fontSize: 18,
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    image: {
+        width: 400,
+        height: 300,
+        resizeMode: 'contain',
+        marginTop: "-10%",
+    },
+    titulo: {
+        fontSize: 22,
         fontFamily: "RobotoRegular",
-        color: "#ffff",
+        color: "black",
+        textAlign: "left", 
+        alignSelf: 'flex-start', // Faz com que o título se alinhe ao início do container pai
+        marginHorizontal: 20, // Adiciona margem lateral para ajustar o espaçamento
+        marginVertical: 8,
     },
     retangulo: {
         padding: 10,
-        borderRadius: 24,
-        margin: 8,
-        backgroundColor: "rgba(163, 21, 54, 0.8)",
+        borderRadius: 15,
+        margin: 10,
+        backgroundColor: "#c2a0a5",
+        alignItems: 'center',
     },
     frase: {
-        fontSize: 18,
+        fontSize: 19,
         fontFamily: "RobotoRegular",
-        color: "#ffff",
-    }
+        color: "#fff",
+        textAlign: "left", 
+        
+    },
+    sobre: {
+        textAlign: "left", 
+        alignSelf: 'flex-start', // Faz com que o título se alinhe ao início do container pai
+        marginHorizontal: 20, // Adiciona margem lateral para ajustar o espaçamento
+        marginVertical: 8,
+    },
+    vermais: {
+        fontSize: 21,
+        fontFamily: "RobotoRegular",
+        color: "black",
+        marginTop: 20,
+        
+    },
+    button: {
+        backgroundColor: '#ac0c24', // Cor de fundo do botão
+        paddingVertical: 8,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        marginTop: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+        fontFamily: "RobotoMedium",
+    },
 });
