f86feb175d14af35e8469d9893b1ad9d8e763531
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-09-21 02:53:42 +0000

    [feat] estilizando terminar

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -2,44 +2,48 @@ import { router } from "expo-router";
 import { Button, Text, View, StyleSheet, Image } from "react-native";
 
 export default function About() {
-    return(
-        
-        <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-             <Image
-                        source={require('../assets/images/logocabanni.png')}
-                        style={{
-                            flex: 1,
-                            width: '100%',
-                            height: '100%',
-                            resizeMode: 'contain',
-                        }}
-
-                    />
-            <View style={styles.page}>
-            <Text style={styles.frase}>No aplicativo Cabanni, você encontrará uma seleção refinada de produtos da linha Charmm, incluindo hidratantes e sabonetes exclusivos que elevam a sua experiência de cuidado pessoal.</Text>
-                </View>
-            <Button title="Voltar" onPress={() => {router.replace("/")}} />
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Image
+                source={require('../assets/images/logocabanni.png')}
+                style={{
+                    marginTop: "-90%",
+                    width: '100%', 
+                    height: undefined,
+                    aspectRatio: 1, // Preserva a proporção da imagem
+                    resizeMode: 'contain',
+                }}
+            />
+            <View>
+            <Text style={styles.titulo}>
+                Seja bem-vindo !
+                </Text>
+            </View>
+            <View style={styles.retangulo}>
+                <Text style={styles.frase}>
+                    No aplicativo Cabanni, você encontrará uma seleção refinada de produtos da linha Charmm, incluindo hidratantes e sabonetes exclusivos que elevam a sua experiência de cuidado pessoal.
+                </Text>
+            </View>
+            <Button title="Voltar" onPress={() => { router.replace("/") }} />
         </View>
     );
 }
+
 const styles = StyleSheet.create({
-    page: {
-        justifyContent: 'center',
-        alignItems: 'center',
+    frase: {
+        fontSize: 18,
+        fontFamily: "RobotoRegular",
+        color: "#ffff",
+    },
+    retangulo: {
         padding: 10,
         borderRadius: 24,
         margin: 8,
-        marginBottom: "80%",
-        borderColor: '#a31536',
-        borderWidth: 1, 
-        backgroundColor: "rgba(163, 21, 54, 0.8)",  
-        
+        backgroundColor: "rgba(163, 21, 54, 0.8)",
     },
     frase: {
-        fontSize: 20,
+        fontSize: 18,
         fontFamily: "RobotoRegular",
         color: "#ffff",
-
-    },
-
-    });
+    }
+});
