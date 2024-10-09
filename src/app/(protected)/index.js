34d7bd5d4091931ce10c0a26b30229bd5b8b5aa2
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-10-09 15:52:33 +0000

    [feat] arrumar botao

--- src/app/(protected)/index.js
+++ src/app/(protected)/index.js
@@ -1,34 +1,46 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, TextInput, ScrollView, TouchableOpacity, Image } from "react-native";
+import { View, StyleSheet, Text, TextInput, ScrollView, TouchableOpacity, Image, Button, Alert } from "react-native";
 import { Banner } from "../../components/Banner";
-import { AntDesign } from '@expo/vector-icons'; 
-import { useNavigation } from '@react-navigation/native';  
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { useCart } from '../../hooks/Cart';
+import { router } from "expo-router";
 
 
 export default function Promocao() {
     const [selectedLink, setSelectedLink] = useState(0);
-    const navigation = useNavigation();  
+    const navigation = useNavigation();
+    const { addCart } = useCart();
 
     const handlePress = (index) => {
         setSelectedLink(index);
-        
+
         if (index === 0) {
-            navigation.navigate('index') ; 
+            router.push("sabonete")
         } else if (index === 1) {
-            navigation.navigate('sabonete'); 
+            navigation.navigate('sabonete');
         } else if (index === 2) {
-            navigation.navigate('list'); 
+            navigation.navigate('list');
         }
     };
 
     const links = ["Produtos", "Sabonete", "Hidratante"];
 
-   
+    const addSaboneteLiquido = () => {
+        addCart({ id: 1, imagemproduto: "", name: "Sabonete Liquido", especificacaoproduto: "", precoproduto: 29.9, quantity: 1, value: 29.9 })
+        Alert.alert("Sabonete Liquido Adicionado");
+    }
+
+    const addHidratante = () => {
+        addCart({ id: 2, imagemproduto: "", name: "Hidrante", especificacaoproduto: "", precoproduto: 49.9, quantity: 1, value: 49.9 })
+        Alert.alert("Hidratante Adicionado");
+    }
+    
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
+            <Button title="adicinar" onPress={addSaboneteLiquido} />
             <View style={styles.containerpesquisa}>
                 <TextInput
                     style={styles.pesquisar}
@@ -36,33 +48,25 @@ export default function Promocao() {
                     keyboardType="default"
                 />
                 <AntDesign name="search1" size={18} color="black" style={styles.iconepesquisa} />
-            </View> 
-            
-            <View style={styles.bannerContainer}>
-                <Banner style={styles.banner} />
             </View>
-     
+
+            <Banner style={styles.banner} />
+
             <View style={styles.containertitulo}>
                 <Text style={styles.titulo}>Encontre a categoria perfeita para você</Text>
             </View>
 
-            {/* Links */}
             <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollContainer}>
-                {links.map((link, index) => (
-                    <TouchableOpacity 
-                        key={index} 
-                        style={[styles.linkContainer, 
-                            { 
-                                backgroundColor: selectedLink === index ? '#ccc' : 'transparent',
-                                borderColor: '#ac0c24',
-                                borderWidth: selectedLink === index ? 0 : 1,
-                            }
-                        ]}
-                        onPress={() => handlePress(index)}  
-                    >
-                        <Text style={styles.link}>{link}</Text>
-                    </TouchableOpacity>
-                ))}
+                <TouchableOpacity
+                    style={styles.linkContainer}
+                    onPress={() => router.push("sabonete")}>
+                    <Text style={styles.link}>Sabonete</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.linkContainer}
+                    onPress={() => router.push("list")}>
+                    <Text style={styles.link}>Hidratante</Text>
+                </TouchableOpacity>
             </ScrollView>
 
             <View style={styles.produtoContainer}>
@@ -82,9 +86,12 @@ export default function Promocao() {
                         <Text style={styles.especificacao}>
                             Com uma incrível fragância de cereja, disponível em 250ml.
                         </Text>
-                        <Text style={styles.preco}>R$ 29,90</Text>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <Text style={styles.preco}>R$ 29,90</Text>
+                            <Button title="+" onPress={addSaboneteLiquido} />
+                        </View>
                     </View>
-                   
+
                 </View>
 
                 <View style={styles.containerproduto2}>
@@ -98,9 +105,12 @@ export default function Promocao() {
                         <Text style={styles.marca2}>CHARMM</Text>
                         <Text style={styles.nomeProduto2}>Sabonete Líquido</Text>
                         <Text style={styles.especificacao2}>
-                        Com aroma de baunilha disponível em 200ml.
+                            Com aroma de baunilha disponível em 200ml.
                         </Text>
-                        <Text style={styles.preco2}>R$ 49,90</Text>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <Text style={styles.preco}>R$ 49,90</Text>
+                            <Button title="+" onPress={addHidratante} />
+                        </View>
                     </View>
                 </View>
             </View>
@@ -113,13 +123,13 @@ export default function Promocao() {
                     />
                 </View>
                 <View style={styles.informacoes3}>
-                <View style={styles.containerpromocao3}>
+                    <View style={styles.containerpromocao3}>
                         <Text style={styles.promocao3}>Promoção</Text>
                     </View>
                     <Text style={styles.marca3}>CHARMM</Text>
                     <Text style={styles.nomeProduto3}>Hidratante Corporal</Text>
                     <Text style={styles.especificacao3}>
-                    Hidratação intensa, com uma delicada fragrância de cereja e lavanda. Disponível em embalagem de 200ml.
+                        Hidratação intensa, com uma delicada fragrância de cereja e lavanda. Disponível em embalagem de 200ml.
                     </Text>
                     <Text style={styles.preco3}>R$ 29,90</Text>
                 </View>
@@ -136,38 +146,38 @@ const styles = StyleSheet.create({
         paddingBottom: 60,
     },
     containerpesquisa: {
-        flexDirection: 'row', 
+        flexDirection: 'row',
         alignItems: 'center',
-        width: '100%', 
+        width: '100%',
         borderColor: '#eee',
         borderWidth: 1,
-        borderRadius: 15, 
-        paddingHorizontal: 8, 
+        borderRadius: 15,
+        paddingHorizontal: 8,
         marginBottom: 10,
     },
     pesquisar: {
-        flex: 1, 
-        height: 40, 
+        flex: 1,
+        height: 40,
         paddingHorizontal: 10,
-        fontSize: 14, 
+        fontSize: 14,
     },
     iconepesquisa: {
         marginLeft: 6,
     },
     bannerContainer: {
-        width: '100%', 
-        height: 250, 
-        marginBottom: 10, 
+        width: '100%',
+        height: 250,
+        marginBottom: 10,
     },
     banner: {
-        width: '100%', 
-        height: '100%', 
-        resizeMode: 'cover', 
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
     },
     containertitulo: {
         marginTop: "-2%",
         marginBottom: "3%",
-    },  
+    },
     titulo: {
         fontSize: 18,
         fontWeight: "400",
@@ -177,7 +187,7 @@ const styles = StyleSheet.create({
         marginTop: "-1%",
     },
     linkContainer: {
-        paddingVertical: 8, 
+        paddingVertical: 8,
         paddingHorizontal: 12,
         height: 40,
         borderRadius: 10,
@@ -186,8 +196,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     link: {
-        color: '#ac0c24',  
-        fontSize: 14, 
+        color: '#ac0c24',
+        fontSize: 14,
         fontFamily: "RobotoMedium",
     },
     produtoContainer: {
@@ -202,7 +212,7 @@ const styles = StyleSheet.create({
         width: '48%',
     },
     containerimagem: {
-        backgroundColor: "#eee", 
+        backgroundColor: "#eee",
         borderRadius: 5,
         alignItems: 'center',
         justifyContent: 'center',
@@ -215,14 +225,14 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         resizeMode: 'contain',
         marginLeft: "-10%",
-    },  
+    },
     imagem2: {
         width: '210%',
         height: '210%',
         borderRadius: 10,
         resizeMode: 'contain',
         marginLeft: "10%",
-    },  
+    },
     containerpromocao1: {
         backgroundColor: '#3baf2c',
         borderRadius: 20,
@@ -243,25 +253,25 @@ const styles = StyleSheet.create({
     informacoes2: {
         marginTop: "2%",
     },
-    marca:{
+    marca: {
         fontSize: 12,
         fontFamily: "OpenSansMedium",
         color: 'black',
         marginBottom: 5,
     },
-    marca2:{
+    marca2: {
         fontSize: 12,
         fontFamily: "OpenSansMedium",
         color: 'black',
         marginBottom: 5,
     },
-    nomeProduto:{
+    nomeProduto: {
         fontSize: 15,
         fontFamily: "OpenSansMedium",
         color: 'black',
         marginBottom: 5,
     },
-    nomeProduto2:{
+    nomeProduto2: {
         fontSize: 15,
         fontFamily: "OpenSansMedium",
         color: 'black',
@@ -284,6 +294,7 @@ const styles = StyleSheet.create({
         fontFamily: "RobotoRegular",
         color: '#8B004C',
         fontWeight: '600',
+        flex: 1
     },
     preco2: {
         fontSize: 16,
@@ -291,23 +302,23 @@ const styles = StyleSheet.create({
         color: '#8B004C',
         fontWeight: '600',
     },
-   
+
     // produto 3
     containerproduto3: {
-        width: '48%',  
-        marginTop: '10%', 
+        width: '48%',
+        marginTop: '10%',
     },
-    
+
     imagem3: {
         width: '230%',
         height: '230%',
         borderRadius: 10,
         resizeMode: 'contain',
         marginLeft: "-6%",
-        marginTop: '16%', 
+        marginTop: '16%',
 
     },
-    
+
     containerpromocao3: {
         backgroundColor: '#3baf2c',
         borderRadius: 20,
@@ -316,7 +327,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         position: "absolute",
-        top: "-90%", 
+        top: "-90%",
         left: 4,
     },
     promocao3: {
@@ -326,13 +337,13 @@ const styles = StyleSheet.create({
         marginTop: "3%",
 
     },
-    marca3:{
+    marca3: {
         fontSize: 12,
         fontFamily: "OpenSansMedium",
         color: 'black',
         marginBottom: 5,
     },
-    nomeProduto3:{
+    nomeProduto3: {
         fontSize: 15,
         fontFamily: "OpenSansMedium",
         color: 'black',
