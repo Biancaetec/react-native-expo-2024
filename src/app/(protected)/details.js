b484932458b7234cc06f146a34ed1073d5564910
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2024-11-06 14:11:01 +0000

    [feat] aula 21 6min

--- src/app/(protected)/details.js
+++ src/app/(protected)/details.js
@@ -1,11 +1,28 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Details() {
     const { id } = useLocalSearchParams();
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={{ flex: 1}}>
             <Text>Detalhes - {id ? id : "Sem id"}  </Text>
+            <View>
+                <Text>Nome</Text>
+                <Text>Data do Pagamento</Text>
+                <Text>Número do Recibo</Text>
+                <Text>Valor Pago</Text>
+                <Text>Observação</Text>
+            </View>
+            <View>
+                <Text> A imagem não foi cadastrada</Text>
+            </View>
+            <View>
+                <Button title="Editar" />
+                <Button title="Definir imagem" />
+                <Button title="Remover imagem" />
+                <Button title="Voltar" />
+            </View>
         </View>
-    );
+        
+);
 }
